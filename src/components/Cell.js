be352a64de560da476969797bd1cc2c467af0c36
Kunal Gursahani <kunalg.dev@gmail.com>
2020-04-01 17:57:39 -0700

    added infocard

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,13 +3,46 @@ import { connect } from "react-redux";
 import {showMessage, showRecord} from "../actions/messageActions";
 import Button from "@material-ui/core/Button";
 import InfoCard from "./InfoCard";
+import {makeStyles} from "@material-ui/core/styles";
+import {grey} from "@material-ui/core/colors";
 
+
+const useStyles = makeStyles({
+    div: {
+        position: "absolute"
+    },
+    infoCard: {
+        background: grey[200],
+        color: grey[100],
+        marginLeft: 400 + 'px',
+        display: "none",
+    },
+    btn: {
+        '&hover': {
+            infoCard: {
+                opacity: 1,
+                background: grey[200],
+                color: grey[100],
+                marginLeft: 400 + 'px',
+                // display: "block",
+                position: "relative",
+                zIndex: -1
+            }
+        }
+    },
+
+
+    // Button:hover .overlay {
+    //     display: "block"
+    // }
+})
 const Cell = ({ message, record, showRecord, showMessage }) => {
   const [hovered, setHovered] = useState(false);
   const toggleHover = () => setHovered(!hovered);
+  const classes = useStyles();
   return (
-    <div>
-      <Button variant="contained"
+    <div className={classes.div}>
+      <Button className={classes.btn}  variant="contained"
               onMouseOver={() => showRecord(record)}
               onMouseEnter={toggleHover}
               onMouseLeave={toggleHover}
@@ -17,7 +50,7 @@ const Cell = ({ message, record, showRecord, showMessage }) => {
       >
       </Button>
       {hovered && (
-          <InfoCard style={"display:block"} records={record}/>
+          <InfoCard className={classes.infoCard} records={record}/>
       )}
     </div>
   );
