f2885c140767d57172a73879de7070ee08e02e13
Kunal Gursahani <kunalg.dev@gmail.com>
2020-04-04 20:06:34 -0700

    set selected attribute

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import { connect } from "react-redux";
 import {showMessage, showRecord} from "../actions/messageActions";
-import Button from "@material-ui/core/Button";
+import {Button} from "@material-ui/core";
 import InfoCard from "./InfoCard";
 import {makeStyles} from "@material-ui/core/styles";
 
@@ -12,26 +12,38 @@ const useStyles = makeStyles({
     },
     btn: {
         background: ({statusColor})=> statusColor,
-        minWidth: 25 + 'px',
-        minHeight: 25 + 'px',
-        maxHeight: 25 + 'px',
-        maxWidth: 25 + 'px',
-    },
+        minWidth: '25px',
+        minHeight: '25px',
+        maxHeight: '25px',
+        maxWidth: '25px',
+        borderColor: 'red'
+    },  "&:hover": {
+        //you want this to be the same as the backgroundColor above
+        background: ({statusColor})=> statusColor,
+    }
 });
 
 const Cell = ({ message, record, showRecord, showMessage, statusColor }) => {
   const [hovered, setHovered] = useState(false);
+  const [selected, setSelected] = useState(false);
+
   const toggleHover = () => setHovered(!hovered);
-  const classes = useStyles({
-      statusColor
-  });
+  const classes = useStyles({statusColor});
+
+  const handleClick = () => {
+      setSelected(!selected)
+      showMessage(message);
+      console.log(selected)
+  };
+
   return (
     <div className={classes.div}>
-      <Button className={classes.btn}  variant="contained"
+      <Button className={classes.btn}
+
               onMouseOver={() => showRecord(record)}
               onMouseEnter={toggleHover}
               onMouseLeave={toggleHover}
-              onClick={() => showMessage(message)}
+              onClick={handleClick}
       >
       </Button>
       {hovered && (
