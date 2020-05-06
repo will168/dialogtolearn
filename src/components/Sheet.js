d8fc564eb22dce3fefbddab8d1da2d33ac484ce3
Kunal Gursahani <kunalg.dev@gmail.com>
2020-05-05 23:45:39 -0700

    removed material ui from table

--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { Table,
-    TableCell,
-    TableBody,
-    TableHead,
+    td,
+    tbody,
+    th,
     TableContainer,
-    TableRow
+    tr
 } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import {green, grey, red, } from "@material-ui/core/colors";
@@ -20,8 +20,8 @@ const useStyles = makeStyles({
     headRow: {
         background: grey[400]
     },
-    tableCell: {
-        paddingBottom: '40px',
+    td: {
+        // paddingBottom: '40px',
 
 
     },
@@ -41,34 +41,35 @@ const Sheet= ({ data }) => {
     const classes = useStyles();
 
     return (
-        <TableContainer>
-            <Table>
-                <TableHead>
-                    <TableRow className={classes.headRow}>
-                        <TableCell>Dates</TableCell>
+        <>
+            <table>
+                <tbody>
+                <th>
+                    <tr className={classes.headRow}>
+                        <td>Dates</td>
                         {colNames.map((colName) => {
-                            return (<TableCell>{colName}</TableCell>)
+                            return (<td>{colName}</td>)
                         })}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
+                    </tr>
+                </th>
+
                     {sheetRecords.map((item) =>
                         (item.student).map((stud) => {
                                 return (
                                     <>
-                                        <TableRow>
-                                            <TableCell className={classes.students}>
+                                        <tr>
+                                            <td className={classes.students}>
                                                 {stud.entityName}
-                                            </TableCell>
+                                            </td>
                                             {
                                                 Object.entries(stud.emails).map(
                                                     ([k , mails]) => {
                                             return (<>
-                                                <TableCell size={'medium'} key={k} >
+                                                <td key={k} >
                                                     {mails.map(
                                                             (mail)=>(
 
-                                                        <td style={{padding: '20px'}}>
+                                                        <td style={{padding: '20px', }}>
                                                                     <Cell
                                                                         recordId = {item.id}
                                                                         entity="student"
@@ -80,20 +81,20 @@ const Sheet= ({ data }) => {
                                                          </td>
 
                                                         ))}
-                                                </TableCell>
+                                                </td>
                                                 </>)}
                                                     )}
-                                        </TableRow>
+                                        </tr>
                                         {item.volunteers.map((vol)=>(
-                                                <TableRow className={classes.volRow}>
-                                                    <TableCell key={vol.entityName} className={classes.volunteers}>
+                                                <tr className={classes.volRow}>
+                                                    <td key={vol.entityName} className={classes.volunteers}>
                                                         {vol.entityName}
-                                                    </TableCell>
+                                                    </td>
                                                     {
                                                         Object.entries(vol.emails).map(
                                                             ([k , mails]) => (
                                                                 <>
-                                                                    <TableCell key={k}>
+                                                                    <td key={k}>
                                                                         {Object.values(mails).map(
                                                                             (mail)=>(
                                                                             <td style={{padding: '20px'}}>
@@ -108,17 +109,17 @@ const Sheet= ({ data }) => {
                                                                             </td>
 
                                                                             ))}
-                                                                    </TableCell>
+                                                                    </td>
                                                                         </>)
                                                         )}
-                                                </TableRow>))
+                                                </tr>))
                                         }
                                     </>
                                 )
                         }))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                </tbody>
+            </table>
+        </>
     )
 }
 
